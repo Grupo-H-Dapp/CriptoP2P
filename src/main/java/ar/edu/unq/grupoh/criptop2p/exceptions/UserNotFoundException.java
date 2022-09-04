1dd66aded2e2deff947a4fa5689a1eece1c1eb96
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2022-09-04 19:20:28 -0300

    add exception handler

---
+++ src/main/java/ar/edu/unq/grupoh/criptop2p/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.grupoh.criptop2p.exceptions;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
