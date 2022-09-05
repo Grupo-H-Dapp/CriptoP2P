5e360260123b5c3a3d5c19d75dca3ec15f943cbc
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2022-09-04 22:56:09 -0300

    Update UserTest.java

--- src/test/java/ar/edu/unq/grupoh/criptop2p/UserTest.java
+++ src/test/java/ar/edu/unq/grupoh/criptop2p/UserTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserTest {
     @Test
     void createOneUserSuccessful() {
-        User user1 = new User("pe","argento","asdsadsa@gmail","asdsadsad","aAsadsadsad.","1234567891234567891233","12345678");
+        User user1 = User.build(0,"pe","argento","asdsadsa@gmail","asdsadsad","aAsadsadsad.","1234567891234567891233","12345678");
         assertEquals(user1.getName(),"pe");
         assertEquals(user1.getLastname(),"argento");
         assertEquals(user1.getEmail(),"asdsadsa@gmail");
