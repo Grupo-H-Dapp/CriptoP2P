cb5676d3752a66d88f4126966dd14a5817cfaab4
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2022-09-04 23:16:47 -0300

    Update UserTest.java

--- src/test/java/ar/edu/unq/grupoh/criptop2p/UserTest.java
+++ src/test/java/ar/edu/unq/grupoh/criptop2p/UserTest.java
@@ -7,8 +7,8 @@ public class UserTest {
     @Test
     void createOneUserSuccessful() {
         User user1 = User.build(0,"pe","argento","asdsadsa@gmail","asdsadsad","aAsadsadsad.","1234567891234567891233","12345678");
-        assertEquals(user1.getName(),"pe");
-        assertEquals(user1.getLastname(),"argento");
-        assertEquals(user1.getEmail(),"asdsadsa@gmail");
+        assertEquals("pe",user1.getName());
+        assertEquals("argento",user1.getLastname());
+        assertEquals("asdsadsa@gmail",user1.getEmail());
     }
 }
