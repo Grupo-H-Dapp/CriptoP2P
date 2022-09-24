545fd9325402deb65fad14ed74ec280ea642033c
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2022-09-24 10:59:15 -0300

    agregado un mapper para los patch

---
+++ src/main/java/ar/edu/unq/grupoh/criptop2p/model/mapper/CustomerMapper.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.grupoh.criptop2p.model.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+    void updateCustomerFromDto(CustomerDto dto, @MappingTarget Customer entity);
+}
